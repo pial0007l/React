ce94cf7af04b6abd0c393d7d5dee8e05de86b6e2
Pial, Hamidur Rahaman <pial0007l@gmail.com>
2020-12-02 12:44:48 +1030

    Wirking with state

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,13 +3,24 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    person: [
+      {name: 'Joy', age:26},
+      {name: 'Arif', age:25},
+      {name: 'Pial', age:26 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
+        
         <h1>Hi I'm a react app.</h1>
-        <Person name="Joy" age="26"/>
-        <Person name="Arif" age="25">I am married</Person>
-        <Person name="Pial" age="26"/>
+        <button>Switch Name</button>
+        <Person name={this.state.person[0].name} age={this.state.person[0].age}/>
+        <Person name={this.state.person[1].name} age={this.state.person[1].name}>I am married</Person>
+        <Person name={this.state.person[2].name} age={this.state.person[2].name}/>
+
       </div>
     );
   }
