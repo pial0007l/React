416ab884c8a3113d677cfb649ae5d8f65820a2f5
Pial, Hamidur Rahaman <pial0007l@gmail.com>
2020-12-02 13:48:35 +1030

    Wirking with event hoocks basic

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,39 +1,54 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
+const app = props => {
+  
+  
+    const [personState, setPerson] = useState({
+      person: [
+        {name: 'Joy', age:26},
+        {name: 'Arif', age:25},
+        {name: 'Pial', age:26 }
+      ]
+    });
 
-class App extends Component {
-  state = {
-    person: [
-      {name: 'Joy', age:26},
-      {name: 'Arif', age:25},
-      {name: 'Pial', age:26 }
-    ]
-  }
+    const switchNameHandler = () => {
+      //console.log('Was clicked');
+      //this.state.person[0].name = 'Mahamud Khan Joy';
+      setPerson({person: [
+        {name: 'Mahamud Khan Joy', age:26},
+        {name: 'Arif Rahman', age:25},
+        {name: 'Hamidur Rahaman Pial', age:26 }
+      ]});
+    };
 
-  switchNameHandler = () => {
-    //console.log('Was clicked');
-    //this.state.person[0].name = 'Mahamud Khan Joy';
-    this.setState({person: [
-      {name: 'Mahamud Khan Joy', age:26},
-      {name: 'Arif Rahman', age:25},
-      {name: 'Hamidur Rahaman Pial', age:26 }
-    ]});
-  }
-
-  render() {
     return (
       <div className="App">
         
         <h1>Hi I'm a react app.</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.person[0].name} age={this.state.person[0].age}/>
-        <Person name={this.state.person[1].name} age={this.state.person[1].age}>I am married</Person>
-        <Person name={this.state.person[2].name} age={this.state.person[2].age}/>
+        <button onClick={switchNameHandler}>Switch Name</button>
+        <Person 
+          name={personState.person[0].name} 
+          age={personState.person[0].age}
+        />
+        <Person 
+          name={personState.person[1].name} 
+          age={personState.person[1].age}
+        >
+          I am married
+        </Person>
+        <Person 
+          name={personState.person[2].name} 
+          age={personState.person[2].age}
+        />
 
       </div>
     );
-  }
+  
 }
 
-export default App;
+export default app;
+
+
+
+
