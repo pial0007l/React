372dc6bad51b01f1800de7c5686e771da432f1e7
Pial, Hamidur Rahaman <pial0007l@gmail.com>
2021-03-26 02:12:59 +1030

    pending

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -9,38 +9,61 @@ const app = props => {
         {name: 'Joy', age:26},
         {name: 'Arif', age:25},
         {name: 'Pial', age:26 }
-      ]
+      ],
+      showPersons: true
     });
 
     const switchNameHandler = () => {
-      //console.log('Was clicked');
-      //this.state.person[0].name = 'Mahamud Khan Joy';
       setPerson({person: [
         {name: 'Mahamud Khan Joy', age:26},
         {name: 'Arif Rahman', age:25},
         {name: 'Hamidur Rahaman Pial', age:26 }
       ]});
-    };
+    }
+
+    const nameChangeHandeler = (event) => {
+      setPerson({
+        person: [
+          {name: 'Khan', age:26},
+          {name: event.target.value, age:25},
+          {name: 'Hamidur Rahaman Pial', age:26 }
+        ]
+      });
+    }
+
+    const togglePersonsHandeler= () => {
+        const doseShow = useState.showPersons;
+        setPerson({showPersons: true});
+    }
 
     return (
       <div className="App">
         
         <h1>Hi I'm a react app.</h1>
-        <button onClick={switchNameHandler}>Switch Name</button>
-        <Person 
-          name={personState.person[0].name} 
-          age={personState.person[0].age}
-        />
-        <Person 
-          name={personState.person[1].name} 
-          age={personState.person[1].age}
-        >
-          I am married
-        </Person>
-        <Person 
-          name={personState.person[2].name} 
-          age={personState.person[2].age}
-        />
+        <button 
+            onClick={togglePersonsHandeler}>Switch Name</button>
+        {
+          useState.showPersons === true ?
+            <div>
+                <Person 
+                  name={personState.person[0].name} 
+                  age={personState.person[0].age}
+                />
+                <Person 
+                  name={personState.person[1].name} 
+                  age={personState.person[1].age}
+                  click={switchNameHandler}
+                  changed={nameChangeHandeler}
+                >
+                  I am married
+                </Person>
+                <Person 
+                  name={personState.person[2].name} 
+                  age={personState.person[2].age}
+                />
+            </div> : null
+        }
+        
 
       </div>
     );
